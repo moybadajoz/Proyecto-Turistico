4e2b39b871f9b15d4a78a2e24ad00f9a0bc9660e
moybadajoz <moisesbadajoz36@gmail.com>
2022-12-09 13:29:03 -0600

    cambiado el estilo de actualizar.php a css vanilla

--- actualizar.php
+++ actualizar.php
@@ -1,10 +1,10 @@
 <?php
-    include("conexion.php");
-    $conexion = conectar();
-    $id = $_GET['CUT'];
-    $sql = "SELECT * FROM usuario WHERE id='$CUT'";
-    $query = mysqli_query($conn, $sql);
-    $Nombre = mysqli_fetch_array($query);
+    //include("conexion.php");
+    //$conexion = conectar();
+    //$id = $_GET['CUT'];
+    //$sql = "SELECT * FROM usuario WHERE id='$CUT'";
+    //$query = mysqli_query($conn, $sql);
+    //$Nombre = mysqli_fetch_array($query);
 ?>
 
 <!DOCTYPE html>
@@ -13,18 +13,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style_actualizar.css">
     <title>CRUD PHP - Update</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-        <div class="row">Datos del Alumno</div>
-        <div class="row">
+        <div class="titulo">Datos del Alumno</div>
+        <div class="inputs">
                 <input type="hidden" name="id" value="<?php echo $Nombre['CUT']?>">
-                <input type="text" name="Nombre" class="form-control mb-3" placeholder="Nombre" style="" value="<?php echo $Nombre['Nombre']?>"> 
-                <input type="text" name="Correo" class="form-control mb-3" placeholder="Correo" value="<?php echo $nombre['Correo']?>">
-                <input type="text" name="pass" class="form-control mb-3" placeholder="Contraseña" value="<?php echo $nombre['pass']?>"> 
-                <input type="submit" value="Actualizar" class="btn btn-success" >
+                <input type="text" name="Nombre" class="form" placeholder="Nombre" style="" value="<?php echo $Nombre['Nombre']?>"> 
+                <input type="email" name="Correo" class="form"  placeholder="Correo" value="<?php echo $nombre['Correo']?>">
+                <input type="password" name="pass" class="form" placeholder="Contraseña" value="<?php echo $nombre['pass']?>"> 
+                <input type="submit" value="Actualizar" class="btn" >
         </div>
     </div>    
 </body>
