fd2e4de11a531adb39f8de9434e4a5485c69c408
Donovan Alatorre <chagolladonovan@gmail.com>
2022-12-10 22:58:01 -0600

    modificacion de login v0.2.3 y modificamos update

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usuario</title>
+    <title>LC4F - Login</title>
     <link rel="stylesheet" href="./css/style_login.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- update.php
+++ update.php
@@ -4,12 +4,16 @@
 
     $id = $_POST['id'];
     $nombre = $_POST['nombre'];
-    $apaterno = $_POST['apaterno'];
-    $amaterno = $_POST['amaterno'];
-    $direccion = $_POST['direccion'];
-    $ciudad = $_POST['ciudad'];
+    $imagen = $_POST['imagen'];
+    $horario = $_POST['horario'];
+    $ubicacion = $_POST['ubicacion'];
+    $costo = $_POST['costo'];
+    $publico = $_POST['publico'];
+    $disiplina = $_POST['disiplina'];
+    $tipo_evento = $_POST['tipo-evento'];
 
-    $sql = "UPDATE alumnos SET nombre='$nombre', apaterno='$apaterno', amaterno='$amaterno',direccion='$direccion', ciudad='$ciudad' WHERE id='$id'";
+    $sql = "UPDATE evento SET nombre='$nombre', imagen='$imagen', horario='$horario',ubicacion='$ubicacion', costo='$costo', 
+            publico='$publico', disiplina='$disiplina', tipo_evento='$tipo_evento' WHERE id='$id'";
     $query = mysqli_query($conn, $sql);
     if($query){
         Header("Location: index.php");
