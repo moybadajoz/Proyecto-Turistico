33e4f4eec408d748a2e590be688443abc0d9ec35
CabreraOros281384 <pl.cabreraoros@ugto.mx>
2022-12-11 14:41:08 -0600

    Terminamos el index

--- index.php
+++ index.php
@@ -67,23 +67,28 @@ Este organismo no será competente para conocer de quejas que se originen con mo
     <div class="logo">
         <img src="./img/logo-guanajuato.png" alt="">
     </div>
-    <div class="nav-btns">
-        <a href="#">boton 1</a>
-        <a href="#">boton 2</a>
-        <a href="#">boton 3</a>
-        <a href="#">boton 4</a>
+    <div class="botones">
+      <a class="text" href="#"><font color=#00000>PUBLICO</font></a>
+    </div>
+    <div class="botones">
+      <a href="#" class="text"><font color=#00000>DISCIPLINA</font></a>
+    </div>
+    <div class="botones">
+      <a href="#" class="text"><font color=#00000>TIPO DE EVENTO</font></a>
+    </div>
+    <div class="botones">
+      <a href="" class="text"><font color=#00000>UBICACION</font></a>
+    </div>
+    <div class="dia">
         <h2>
-        <a href="#" title="link to this post"></a>
         <label for="start">
-        <font  color="#0000">Seleccionar dia</font>
+        <font  color="#00000">FECHA</font>
         </label>
         <input type="date" id="start" name="trip-start" value="2018-07-22"min="2022-11-25" max="2024-12-31">
         </h2>
+</div>
     </div>
-    
-    <div class="nav-login">
-        <a href="/login.php">Login</a>
-    </div>
+
 </div>
 <div class="content">
 <div class="general "></div> 
@@ -97,14 +102,82 @@ Este organismo no será competente para conocer de quejas que se originen con mo
          </div>
          </div>
          <p> <font  color="#00000">
-El Poder Público únicamente puede lo que la Ley le concede y el gobernado todo lo que ésta no le prohíbe.
-La Ley establecerá sistemas de impugnación y medios de defensa de los derechos de los particulares frente a los actos de las autoridades.
-La Ley determinará la organización, funcionamiento, competencia y procedimientos de un organismo estatal de protección de los derechos humanos, dotado de plena autonomía, que conocerá de quejas en contra de actos u omisiones de naturaleza administrativa provenientes de cualquier autoridad o servidor público de carácter estatal o municipal que violen estos derechos, formulará acuerdos o recomendaciones públicas, no vinculatorias, denuncias y quejas ante las autoridades respectivas.
-Este organismo no será competente para conocer de quejas que se originen con motivo de acuerdos o decisiones de instancias electorales, ni tratándose de resoluciones de naturaleza jurisdiccional; pero podrá conocer de asuntos de orden administrativo de los órganos de impartición de justicia que transgredan derechos humanos.</font>
-</font></p> 
 
+</font></p>
 
+  <div class="container-card">
+  
+  <div class="card">
+    <figure>
+      <img src="./img/GtoMomias02.jpg">
+    </figure>
+    <div class="contenido-card">
+      <h3>Panteon de Santa Paula</h3>
+      <p>Recorrido por el museo de las momias</p>
+      <a href="#">Información</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <figure>
+      <img src="./img/obra1.jpg">
+    </figure>
+    <div class="contenido-card">
+      <h3>Homilética I</h3>
+      <p>Portal de tareas de Homilética I</p>
+      <a href="#">Acceder</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <figure>
+      <img src="./img/obra2.jpg">
+    </figure>
+    <div class="contenido-card col-4">
+      <h3>Introducción al Antiguo Testamento</h3>
+      <p>Portal de tareas de Introducción al Antiguo Testamento</p>
+      <a href="#">Acceder</a>
+    </div>
+  </div>
+</div>
+
+<div class="container-card">
+
+  <div class="card">
+    <figure>
+      <img src="./img/obra6.jpg">
+    </figure>
+    <div class="contenido-card">
+      <h3>Evangelios</h3>
+      <p>Portal de tareas de Evangelios</p>
+      <a href="#">Acceder</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <figure>
+      <img src="./img/obra4.jpg">
+    </figure>
+    <div class="contenido-card">
+      <h3>Homilética I</h3>
+      <p>Portal de tareas de Homilética I</p>
+      <a href="#">Acceder</a>
+    </div>
+  </div>
+
+  <div class="card">
+    <figure>
+      <img src="./img/obra5.jpg">
+    </figure>
+    <div class="contenido-card col-4">
+      <h3>Introducción al Antiguo Testamento</h3>
+      <p>Portal de tareas de Introducción al Antiguo Testamento</p>
+      <a href="#">Acceder</a>
+    </div>
+  </div>
+</div> 
 
 </div>
+
 </body>
 </html>
